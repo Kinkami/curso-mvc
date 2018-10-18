73db7b9a07dbb1353298d9d302b6862502d1def8
Fernando <fernando.henrique.leme@gmail.com>
2018-10-17 22:35:11 -0300

    Views e retornos
    View bag, view data e tipadas

--- Introducao/Introducao/Controllers/HomeController.cs
+++ Introducao/Introducao/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Introducao.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,21 @@ namespace Introducao.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var pessoa = new Pessoa
+            {
+                PessoaId = 1,
+                Nome = "Fer",
+                Tipo = "Mestre Ursal"
+            };
+            ViewData["PessoaId"] = pessoa.PessoaId;
+            ViewData["Nome"] = pessoa.Nome;
+            ViewData["Tipo"] = pessoa.Tipo;
+
+            ViewBag.PessoaId = pessoa.PessoaId;
+            ViewBag.Nome = pessoa.Nome;
+            ViewBag.Tipo = pessoa.Tipo;
+
+            return View(pessoa);
         }
 
 
@@ -19,6 +34,35 @@ namespace Introducao.Controllers
         {
             return View();
         }
+
+        //[HttpPost]
+        //public ActionResult Lista(int PessoaId, string Nome, string Tipo)
+        //{
+        //    ViewData["PessoaId"] = PessoaId;
+        //    ViewData["Nome"] = Nome;
+        //    ViewData["Tipo"] = Tipo;
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public ActionResult Lista(FormCollection form)
+        //{
+        //    ViewData["PessoaId"] = form["PessoaId"];
+        //    ViewData["Nome"] = form["Nome"]; 
+        //    ViewData["Tipo"] = form["Tipo"]; 
+        //    return View();
+        //}
+
+
+        [HttpPost]
+        public ActionResult Lista(Pessoa p)
+        {
+            ViewData["PessoaId"] = p.PessoaId;
+            ViewData["Nome"] = p.Nome;
+            ViewData["Tipo"] = p.Tipo;
+            return View();
+        }
+
 
 
     }
---
+++ Introducao/Introducao/Models/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Introducao.Models
+{
+    public class Pessoa
+    {
+        public int PessoaId { get; set; }
+        public String Nome { get; set; }
+        public String Tipo { get; set; }
+
+    }
+}
